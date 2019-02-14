8fb00b743de25b13a535aadbb94fa6b810b5a5c1
Michael Samper <msamper@ufl.edu>
2019-02-14 14:20:35 -0500

    fix image routing for windows machines

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -23,7 +23,7 @@ module.exports = {
   output: {
     path: __dirname,
     filename: 'app.js',
-    publicPath: 'http://0.0.0.0:8000/',
+    publicPath: 'http://127.0.0.1:8000/',
   },
 
   resolve: {
