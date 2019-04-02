a53856195c4da4c5413c9cc4c13bbb729e35f76d
Michael Samper <msamper@ufl.edu>
2019-04-02 12:27:32 -0400

    fixed gitignore

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model('User', UserSchema);
